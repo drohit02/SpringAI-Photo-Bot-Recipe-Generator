75ed879994ce5e3f72ea27e84c5d03b72261d2b2
drohit02 <drohit045@gmail.com>
2024-09-12 23:56:19 +0530

    Spring AI project Initial stage

---
+++ Photo-Bot-RecipeGen-SpringAI/src/main/java/com/ai/SpringAI/PhotoBotRecipeGenSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.ai.SpringAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoBotRecipeGenSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoBotRecipeGenSpringAiApplication.class, args);
+	}
+
+}
---
+++ Photo-Bot-RecipeGen-SpringAI/src/test/java/com/ai/SpringAI/PhotoBotRecipeGenSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.SpringAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoBotRecipeGenSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
